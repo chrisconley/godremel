e4302b47957e2ae36d12cf981dfd3a4d831380f2
Chris Conley <chris.m.conley@gmail.com>
2013-06-19 08:47:01 -0400

    working for this subset, but will fail when adding additional fields -need to figure out this writer thing

--- levels.py
+++ levels.py
@@ -1,3 +1,4 @@
+import re
 # procedure DissectRecord(RecordDecoder decoder, FieldWriter writer, int repetitionLevel):
 #     Add current repetitionLevel and definition level to writer
 #
@@ -24,27 +25,37 @@
 
 
 def disect_record(record, schema, columns, rlevel=0):
+    seen_fields = set()
     for key, child_schema in schema.iteritems():
         path = child_schema.get('path')
+        child_rlevel = rlevel
+
         if child_schema['type'] == int:
-            print key, path
             record_value = record.get(path)
-            dlevel = len(path.split('.'))
-            dlevel = dlevel if record_value else dlevel - 1
-            column = (record_value, rlevel, dlevel)
+
+            if re.search('\.', path):
+                dlevel = len(path.split('.'))
+                dlevel = dlevel if record_value else dlevel - 1
+            else:
+                dlevel = 0
+            column = (record_value, child_rlevel, dlevel)
             if record_value:
                 if child_schema.get('repeated'):
                     for val in record_value:
-                        column = (val, rlevel, dlevel)
+                        if path in seen_fields:
+                            child_rlevel = rlevel +  1
+                        else:
+                            seen_fields.add(path)
+                        column = (val, child_rlevel, dlevel)
                         columns[path].append(column)
                 else:
                     columns[path].append(column)
             else:
-                columns[path].append((None, rlevel, dlevel))
+                columns[path].append((None, child_rlevel, dlevel))
 
 
         if child_schema['type'] == 'group':
-            disect_record(record, child_schema['children'], columns)
+            disect_record(record, child_schema['children'], columns, child_rlevel)
 
     return columns
 
@@ -81,7 +92,9 @@ if __name__ == '__main__':
     print '`````'
     disect_record(r2, schema, columns)
 
+    print columns['id']
     print columns['links.forward']
+    print columns['links.backward']
 
 
 
