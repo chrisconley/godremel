244604fafd13adb5d999b655cee183e92687f03f
Chris Conley <chris.m.conley@gmail.com>
2013-07-06 09:26:11 -0400

    started adding fsm constructor in go

--- fsm.go
+++ fsm.go
@@ -42,7 +42,7 @@ func construct(fields [2]Field) map[FsmState]Field {
         // for each preField before field whose repetition level is larger than barrierLevel:
         // Walk each prefield starting with the most recent field
         for preFieldIndex := fieldIndex-1; preFieldIndex >= 0; preFieldIndex-- {
-            fmt.Printf("hi\n")
+            fmt.Printf("preFieldIndex: %s\n", preFieldIndex)
         }
 
         // for each level in [barrierLevel+1..maxLevel] that lacks transition from field:
